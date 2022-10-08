21aee5b5c00c6e6adafbfbfefe47c1f54e45cfb6
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-10-07 20:42:36 -0700

    INITIAL COMMIT: created basic files

---
+++ main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(800, 600)
+screen.title("Python Pong")
+screen.bgcolor("black")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
