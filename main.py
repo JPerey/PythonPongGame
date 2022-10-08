d40ee1f881388140d3d9f6ab780ec89bf613f8da
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-10-08 00:28:52 -0700

    WIP: added paddles and movement for paddles

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
+import time
 
 screen = Screen()
 screen.setup(800, 600)
 screen.title("Python Pong")
 screen.bgcolor("black")
+screen.tracer(0)
+
+paddle1 = Paddle()
+paddle2 = Paddle()
+paddle2.second_paddle()
+
+game_over = False
+screen.listen()
+screen.update()
+
+screen.onkey(paddle1.move_up, "w")
+screen.onkey(paddle1.move_down, "s")
+
+screen.onkey(paddle2.move_up2, "Up")
+screen.onkey(paddle2.move_down2, "Down")
+
+while not game_over:
+    screen.update()
+    time.sleep(.2)
 
 
 
---
+++ paddle.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+PADDLE_MOVE_DISTANCE = 20
+
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.penup()
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.goto(-350, 0)
+        self.y_height = 0
+
+    def second_paddle(self):
+        self.setpos(350, 0)
+
+    def move_up(self):
+        self.y_height += PADDLE_MOVE_DISTANCE
+        self.goto(-350, self.y_height)
+
+    def move_down(self):
+        self.y_height -= PADDLE_MOVE_DISTANCE
+        self.goto(-350, self.y_height)
+
+    def move_up2(self):
+        self.y_height += PADDLE_MOVE_DISTANCE
+        self.goto(350, self.y_height)
+
+    def move_down2(self):
+        self.y_height -= PADDLE_MOVE_DISTANCE
+        self.goto(350, self.y_height)
